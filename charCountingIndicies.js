391a8d2bb8b397b1aaa45b417697db698b7d46aa
Bana <banabatshon@hotmail.com>
2019-03-06 20:30:04 +0000

    fixes the indentation

--- charCountingIndicies.js
+++ charCountingIndicies.js
@@ -1,16 +1,13 @@
-
-
-  function countLetters (word) {
-    var outputObj = {}
-    var wordChars = word.split(" ").join("");
-    for (var i = 0; i < wordChars.length; i++) {
-      if (outputObj[wordChars[i]]) {
-        outputObj[wordChars[i]].push(i);
-      } else {
-        outputObj[wordChars[i]] = [i];
-      }
-    } 
-    return outputObj;
-  };
-  
-    console.log(countLetters("lighthouse in the house"));
+function countLetters (word) {
+  var outputObj = {}
+  var wordChars = word.split(" ").join("");
+  for (var i = 0; i < wordChars.length; i++) {
+    if (outputObj[wordChars[i]]) {
+      outputObj[wordChars[i]].push(i);
+    } else {
+      outputObj[wordChars[i]] = [i];
+    }
+  }
+  return outputObj;
+};
+console.log(countLetters("lighthouse in the house"));
